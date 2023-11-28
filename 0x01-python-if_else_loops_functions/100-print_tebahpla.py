9c44e96aea41d115ccb8a7b0cc688f97f0151525
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-11-28 10:17:14 -0800

    Teapla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print(chr(90 - i) if i % 2 == 0 else chr(122 - i) for i in range(26))
+
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+    print(chr(i), end="")
