8cd2f6bf9b8c962d40ac580e04f0df780aec6db6
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-12-19 07:13:36 -0800

    Update last

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,56 +1,47 @@
 #!/usr/bin/python3
-""" creates class Square """
-
+"""Square class definition"""
 
 class Square:
-    """ Square class"""
+    """Square class"""
+
     def __init__(self, size=0):
-        if type(size) != int and type(size) != float:
-            raise TypeError("size must be a number")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        """Square constructor 
+        Args:
+            size (int): length of a side of Square
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     @property
     def size(self):
+        """The property of size as the length 
+        of a side of Square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int and type(value) != float:
-            raise TypeError("size must be a number")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        """Setter method for size property
+        Args:
+            value (int): new size value
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     def area(self):
+        """Get the area instance for comparators"""
         return self.__size * self.__size
 
-    def __eq__(self, other):
-        """check if equal to another square"""
-        if not isinstance(other, Square):
-            return False
-        return self.area() == other.area()
-
-    def __lt__(self, other):
-        """check if less than other square"""
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        """check if less than or equal to other square"""
-        return self.area() <= other.area()
-
-    def __ne__(self, other):
-        """check if not equal to another square"""
-        return self.area() != other.area()
-
-    def __gt__(self, other):
-        """check if greater than another square"""
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        """check if greater than or equal to another square"""
-        return self.area() >= other.area()
-
+    # ... (comparator methods)
