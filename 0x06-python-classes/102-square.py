551a584f472e5ceb19ab31f9600d578b70c9b25f
MutsotsoDennis <mutsotsodennis90@gmail.com>
2023-12-19 06:38:55 -0800

    Magic class and square 101-102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,52 +1,53 @@
 #!/usr/bin/python3
-"""Square class defination"""
+""" creates class Square """
 
 
 class Square:
-    """square body"""
-
+    """ Square class"""
     def __init__(self, size=0):
-        """square contructor 
-        Args: size: length of a side of Square
-        """
-        self.__size = size
+        if type(size) != int and type(size) != float:
+            raise TypeError("size must be a number")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
-        """"The propery of size as the length 
-        of a side of Square
-        Raises:
-            TypeError: if size != int
-            ValueErrorr: if size < 0
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+        if type(value) != int and type(value) != float:
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
-        """Get the area instance to comparators"""
         return self.__size * self.__size
 
-    def __le__(self, other):
-        return self.area() <= other.area()
+    def __eq__(self, other):
+        """check if equal to another square"""
+        return(self.area() == other.area())
 
     def __lt__(self, other):
-        return self.area() < other.area()
+        """check if less than other square"""
+        return(self.area() < other.area())
 
-    def __ge__(self, other):
-        return self.area() >= other.area()
+    def __le__(self, other):
+        """check if less than or equal to other square"""
+        return(self.area() <= other.area())
 
     def __ne__(self, other):
-        return self.area() != other.area()
+        """check if not equal to another suqare"""
+        return(self.area() != other.area())
 
     def __gt__(self, other):
-        return self.area() > other.area()
+        """check if greater than another square"""
+        return(self.area() > other.area())
 
-    def __eq__(self, other):
-        return self.area() == other.area()
+    def __ge__(self, other):
+        """check if greater than or equal to another square"""
+        return(self.area() >= other.area())
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,23 +1,23 @@
-
-
 #!/usr/bin/python3
-"""Magic class from a given ByteCode."""
+"""magic class definition"""
 import math
 
 
 class MagicClass:
-    """Initialization of the MagicClass."""
+    """magicclass that makes bytecode """
+
     def __init__(self, radius=0):
-        """magic class constructor"""
-        self._MagicClass__radius = 0
+        """Initialize class"""
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
-        self._MagicClass__radius = radius
+            raise TypeError('radius must be a number')
+        else:
+            self.__radius = radius
 
     def area(self):
-        """Calculation of the area."""
-        return self._MagicClass__radius ** 2 * math.pi
+        """function that calculates area """
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """Calculation of the circumference."""
-        return 2 * math.pi * self._MagicClass__radius
+        """function that calculates circumference"""
+        return (2 * math.pi) * self.__radius
